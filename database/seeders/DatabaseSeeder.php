4f182446918f101a886e442687ab1e3712cf9de1
Yordanos <yordinegusu21@gmail.com>
2024-08-09 18:00:56 +0300

    created a listingFactory and updated the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,28 +15,29 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        Listing::factory(6)->create();
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        Listing::create([
-            'title' => 'laravel senior developer',
-            'tags' => 'aravel, javascriptl',
-            'company' => 'yo corp',
-            'location' => 'bole',
-            'email' => 'yo@gmail.com',
-            'website' => 'https://www.yo.com',
-            'description' => 'fdbvgdfhxdhhbxhbfvxhjvhjcxvhbchbfjvjvj'
-        ]);
-        Listing::create([
-            'title' => 'laravel junior developer',
-            'tags' => 'php, javascriptl',
-            'company' => 'newtech corp',
-            'location' => 'bole',
-            'email' => 'newtech@gmail.com',
-            'website' => 'https://www.newtech.com',
-            'description' => 'fdbvgdfhxdhhbxhbfvxhjvhjcxvhbchbfjvjvj'
-        ]);
+        // Listing::create([
+        //     'title' => 'laravel senior developer',
+        //     'tags' => 'aravel, javascriptl',
+        //     'company' => 'yo corp',
+        //     'location' => 'bole',
+        //     'email' => 'yo@gmail.com',
+        //     'website' => 'https://www.yo.com',
+        //     'description' => 'fdbvgdfhxdhhbxhbfvxhjvhjcxvhbchbfjvjvj'
+        // ]);
+        // Listing::create([
+        //     'title' => 'laravel junior developer',
+        //     'tags' => 'php, javascriptl',
+        //     'company' => 'newtech corp',
+        //     'location' => 'bole',
+        //     'email' => 'newtech@gmail.com',
+        //     'website' => 'https://www.newtech.com',
+        //     'description' => 'fdbvgdfhxdhhbxhbfvxhjvhjcxvhbchbfjvjvj'
+        // ]);
     }
 }
