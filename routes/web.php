0d5ec5e0ba79d5f211d189464913af98b4f96d3e
Yordanos <yordinegusu21@gmail.com>
2024-08-11 15:57:07 +0300

    updating the tags component, created a listing controller

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\listing;
+use App\Http\Controllers\ListingController;
+
+//common resource routes
+//index
+//show
+//create - show a form to create a new listing
+//store - store new listing
+//edit - show a form to edit a listing
+//update -update a listing
+//destroy -delete listing
+
 //All listings
-Route::get('/', function () {
-   return view('listings',
-   [
-    'heading' => 'latest listings',
-    'listings' => Listing::all()
-   ]);
-});
-Route::get('/listings/{id}', function($id)
-{
-return view('listing',[
-        'listing' => Listing::find($id)
-]);
-});
-Route::get('/test-db', function () {
-    $listings = Listing::all();
-    return $listings;
-});
+Route::get('/', [ListingController::class, 'index']);
+//single listing
+
+Route::get('/listings/{listing}', [ListingController::class,'show']);
