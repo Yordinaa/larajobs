c35a69d51b6be8ef9acb3a51eb3f29f6d1935575
Yordanos <yordinegusu21@gmail.com>
2024-08-12 03:44:48 +0300

    additional features edit and delete lisings

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,17 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 //store listing data
 
 Route::post('/listings', [ListingController::class,'store']);
+
+//show edit form
+Route::get('/listings/{listing}/edit',[ListingController::class, 'edit']);
+
+//submit the edit form to update
+Route::put('/listings/{listing}',[ListingController::class, 'update']);
+
+
+//Delete
+Route::delete('/listings/{listing}',[ListingController::class, 'destroy']);
+
 //single listing
 
 Route::get('/listings/{listing}', [ListingController::class,'show']);
