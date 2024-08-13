564c8f10f32ff6c2784d48e816c44cdc2a898349
Yordanos <yordinegusu21@gmail.com>
2024-08-13 18:41:26 +0300

    updated theme

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="images/favicon.ico" />
+        <link rel="icon" href="themes/images/favicon.ico" />
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
@@ -27,7 +27,7 @@
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
-            <a href="/"><img class="w-24" src="{{ asset('themes/images/logo.png')}}" alt="" class="logo"/></a>
+            <a href="/"> <img class="w-24" src="{{ asset('themes/images/logo.png')}}" alt="" class="logo"/></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 @auth
 
